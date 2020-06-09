fc47d8e38e4723845a36dcc7cd6f427a48ef4bde
brutal-snezhok <czyrulik97@mail.ru>
2020-06-09 16:49:13 +0300

    Revert "solved issue with injecting prototype in singleton with application context injection"
    This reverts commit 6b2da2d2

--- src/main/java/com/tsyrulik/OrderValidator.java
+++ src/main/java/com/tsyrulik/OrderValidator.java
@@ -1,29 +1,24 @@
 package com.tsyrulik;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-public class OrderValidator implements ApplicationContextAware {
+public class OrderValidator {
     private int minOrderNumber = 1;
 
-    private ApplicationContext applicationContext;
+    private final NonThreadSafeValidator nonThreadSafeValidator;
+
+    public OrderValidator(NonThreadSafeValidator nonThreadSafeValidator) {
+        this.nonThreadSafeValidator = nonThreadSafeValidator;
+    }
 
     public boolean isValid(Order order) {
         if(order.getOrderNumber() < minOrderNumber) {
             return true;
         }
-        applicationContext.getBean(NonThreadSafeValidator.class).isValid(order);
+        nonThreadSafeValidator.isValid(order);
 
         return false;
     }
 
     public void setMinOrderNumber(int minOrderNumber) {
         this.minOrderNumber = minOrderNumber;
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
     }
 }
